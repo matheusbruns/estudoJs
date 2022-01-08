e087312d39badc3cb6c8b81cd73c79b4586d703a
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-07 23:41:49 -0300

    Relógio atualizando em tempo real

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ function carregar(){
   var data = new Date()
   var hora = data.getHours()
   var min = data.getMinutes()
+  var seg = data.getSeconds()
+
+  var tempoTotal = hora + ":" + min + ":" + seg 
 
   if(hora >= 0 && hora <12){
-    msg.innerHTML = `<strong>Bom dia</strong> <br> agora são ${hora} : ${min}`
+    msg.innerHTML = `<strong>Bom dia</strong> <br> agora são ${tempoTotal}`
     img.src = 'manha.png'
     document.body.style.background = 'rgb(147, 190, 207)'
   }else if(hora >= 12 && hora <= 18){
-    msg.innerHTML = `<strong>Boa tarde</strong> <br> agora são ${hora} : ${min}`
+    msg.innerHTML = `<strong>Boa tarde</strong> <br> agora são ${tempoTotal}`
     img.src = 'tarde.png'
     document.body.style.background = 'rgb(84, 1, 109)'
   }else{
-    msg.innerHTML = `<strong>Boa noite</strong> <br> agora são ${hora} : ${min}`
+    msg.innerHTML = `<strong>Boa noite</strong> <br> agora são ${tempoTotal}`
     img.src = 'noite.png'
     document.body.style.background = 'rgb(0, 45, 129)'
 
   }
 
 }
+setInterval(carregar,500) //atualizando em tempo real
