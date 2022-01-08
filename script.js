76f0b08f1c0586ed5ddc226d6876432eb5e9240d
Matheus Bruns <matheus.rbruns@gmail.com>
2022-01-07 22:53:46 -0300

    arrumando o local das imagens

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function carregar(){
   var data = new Date()
   var hora = data.getHours()
   var min = data.getMinutes()
-  
+
   if(hora >= 0 && hora <12){
     msg.innerHTML = `<strong>Bom dia</strong> <br> agora são ${hora} : ${min}`
-    img.src = '../images/manha.png'
+    img.src = 'manha.png'
     document.body.style.background = 'rgb(147, 190, 207)'
   }else if(hora >= 12 && hora <= 18){
     msg.innerHTML = `<strong>Boa tarde</strong> <br> agora são ${hora} : ${min}`
-    img.src = '../images/tarde.png'
+    img.src = 'tarde.png'
     document.body.style.background = 'rgb(84, 1, 109)'
   }else{
     msg.innerHTML = `<strong>Boa noite</strong> <br> agora são ${hora} : ${min}`
-    img.src = '../images/noite.png'
+    img.src = 'noite.png'
     document.body.style.background = 'rgb(0, 45, 129)'
 
   }
